f47d3ae85bc9f18211b91e4e1d8b244ca567ef4d
Marco <marcoasjunior90@gmail.com>
2020-06-30 19:49:09 -0300

    first screen - home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <vs-row>
+      <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-w="12">
+      <img alt="Vue logo" src="../assets/logo.png">
+      </vs-col>
+    </vs-row>
+    <vs-row>
+      <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-w="12">
+      <vs-button color="primary" class="button" type="gradient">Entrar</vs-button>
+      </vs-col>
+    </vs-row>
+    <vs-row>
+      <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-w="12">
+      <vs-button color="primary" class="button" type="gradient">Cadastre-se</vs-button>
+      </vs-col>
+    </vs-row>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+
+  name: 'Home'
+
 }
 </script>
+
+<style scoped>
+
+.button {
+  width: 80%;
+  margin: 10px;
+}
+
+img {
+  margin-top: 20%;
+}
+
+</style>

aad44a952e93b47ac87076723e2fc288eada35c5
Marco <marcoasjunior90@gmail.com>
2020-06-30 18:51:01 -0300

    set inicial com vuesax

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import Vuesax from 'vuesax'
+import 'vuesax/dist/vuesax.css'
+
+Vue.use(Vuesax, {
+  // options here
+})
 
 Vue.config.productionTip = false
 

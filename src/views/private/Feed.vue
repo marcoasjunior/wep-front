e2d56a322ca1ccdbe95c7a42ae086098110c38fe
Pedro Lopes <pedro@stratum.bt>
2020-07-04 20:31:51 -0300

    event Card and styles

--- src/views/private/Feed.vue
+++ src/views/private/Feed.vue
@@ -1,5 +1,24 @@
 <template>
     <div>
         <h1>Feed</h1>
+
+        <div class="p15">
+            <v-card max-width="500" class="ac card-style-1">
+                <img :src='imageURL' class="img-size">
+                
+                <div class="p10 alg-txt-s">
+                    <h4 class="clr-red">SEX, 07 FEV - 20:00 ás 23:30</h4>
+                    <h1 class="mt-1">Design Party</h1>
+                    <span class="mt-2 display-b">Criado por <strong>Pedro Lopes</strong> </span>
+                </div>
+            </v-card>
+        </div>
     </div>
 </template>
+<script>
+export default {
+    data:() => ({
+        imageURL:'./img-card-0.png'
+    })
+}
+</script>

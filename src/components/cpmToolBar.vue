53054a241b14db292827bf4d1915377d056588f4
Pedro Lopes <pedro@stratum.bt>
2020-07-09 20:59:32 -0300

    styles and tool bar

---
+++ src/components/cpmToolBar.vue
@@ -0,0 +1,59 @@
+<template>
+  <!-- <v-card
+    class="mx-auto overflow-hidden"
+    height="400"
+  > -->
+<div class="toolBardiv">
+    <v-app-bar color="deep-purple" dark max-width="800" class="ac">
+        <div class="d-flex flex-row justify-space-between">
+
+        <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+        <v-toolbar-title></v-toolbar-title>
+      
+        </div>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Account</v-list-item-title>
+          </v-list-item>
+
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  <!-- </v-card> -->
+
+</div>
+
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: false,
+    }),
+  }
+</script>
--- src/views/private/Feed.vue
+++ src/views/private/Feed.vue
@@ -1,24 +1,40 @@
 <template>
     <div>
+        <ToolBar/>
         <h1>Feed</h1>
 
-        <div class="p15">
+        <div class="p15 mt-10" v-for="(item, i) in itens" :key="i">
             <v-card max-width="500" class="ac card-style-1">
-                <img :src='imageURL' class="img-size">
+                <img :src='item.imageEvent' class="img-size">
                 
                 <div class="p10 alg-txt-s">
-                    <h4 class="clr-red">SEX, 07 FEV - 20:00 ás 23:30</h4>
-                    <h1 class="mt-1">Design Party</h1>
-                    <span class="mt-2 display-b">Criado por <strong>Pedro Lopes</strong> </span>
+                    <h4 class="clr-red">{{ item.date }}</h4>
+                    <h1 class="mt-1">{{ item.title }}</h1>
+                    <span class="mt-2 display-b">Criado por <strong>{{ item.user }}</strong> </span>
                 </div>
             </v-card>
         </div>
     </div>
 </template>
 <script>
+import ToolBar from '@/components/cpmToolBar'
+
 export default {
     data:() => ({
-        imageURL:'./img-card-0.png'
-    })
+        imageURL:'./img-card-0.png',
+
+        itens:[
+            {id:'1',date:'SEX, 07 FEV - 20:00 ás --:--', title:'Design Party', user:'Pedro Lopes', imageEvent:'./img-card-0.png'},
+            {id:'2',date:'SEG, 12 JAN - 19:40 ás --:--', title:'Design Party', user:'Marco Antônio', imageEvent:'./img-card-0.png'},
+            {id:'3',date:'TER, 23 DEZ - 21:30 ás 23:30', title:'Design Party', user:'Liam Cabral', imageEvent:'./img-card-0.png'},
+            {id:'4',date:'SEX, 07 FEV - 20:00 ás 23:30', title:'Design Party', user:'Pedro Lopes', imageEvent:'./img-card-0.png'},
+            {id:'5',date:'SEX, 07 FEV - 20:00 ás 23:30', title:'Design Party', user:'Pedro Lopes', imageEvent:'./img-card-0.png'},
+        ]
+    }),
+
+
+    components:{
+        ToolBar
+    }
 }
 </script>

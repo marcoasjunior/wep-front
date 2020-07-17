caef27523797798c991342e4816d8e952ecd8412
Liam Cabra Teixeira <liam_texeira@estudante.sc.senai.br>
2020-07-16 21:51:01 -0300

    +Profile(cards preferencia de events e raio de procura)

--- src/views/private/Profile.vue
+++ src/views/private/Profile.vue
@@ -10,15 +10,47 @@
                     <h2 class="alg-txt-l ml-2">Foto de perfil</h2>
 
                     <v-col class="d-flex justify-center align-center">
-                        <v-avatar class="mb-5 cp" size="80">
+                        
+                        <v-avatar class="mb-5 cp" size="80">           
                             <img
                                 src="https://cdn.vuetifyjs.com/images/john.jpg"
                                 alt="John"
                             >
                         </v-avatar>
+
+                        <v-file-input hide-input="">
+                            <button
+                                type="button"
+                                aria-label="prepend-icon"
+                                class="v-icon notranslate v-icon--link mdi mdi-paperclip theme--light"
+                            >
+                            </button>
+                        </v-file-input>   
                     </v-col>
+                </v-card>   
+
+                <v-card class="mb-3">
+                    <h2 class="alg-txt-l ml-2">Preferência de eventos</h2>
+                    <v-container fluid>
+                        <v-row align="center">
+                            <v-col>
+                                <v-select
+                                    v-model="MyEventTypes"
+                                    :items="AllEventTypes"
+                                    attach
+                                    chips
+                                    label="Escolha sua preferência"
+                                    multiple
+                                ></v-select>
+                            </v-col>
+                        </v-row>
+                    </v-container>
                 </v-card>
 
+                <div class="mb-3">
+                    <h2 class="alg-txt-l ml-2">Raio de procura dos eventos</h2>
+                    <SetMap/>
+                </div>
 
                 <v-card class="mb-3">
                     <h2 class="alg-txt-l ml-2">Segurança</h2>
@@ -54,8 +86,6 @@
                     </v-col>
                 </v-card>
 
-                
-
                 <div>
                     <v-col class="text-center mt-7" cols="1" sm="12">
                         <v-btn
@@ -75,10 +105,17 @@
 
 <script>
 import Toolbar from '@/components/cpmToolBar';
+import SetMap from '../../components/cpmSetMapPoints';
 
 export default {
     components: {
         Toolbar,
-    }
+        SetMap
+    },
+
+    data: () => ({
+        AllEventTypes: ["Rock", "Funk", "Lual", "Gospel"],
+        MyEventTypes: []
+    }),
 }
 </script>
